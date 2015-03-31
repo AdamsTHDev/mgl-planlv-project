6a100eda6871d9dda6602b9bba42201c07a4c29e
PataweeCAdmsTHDev <moth.pc@gmail.com>
2015-03-31 10:37:38 +0700

    Add method - Create Directory

--- src/test/java/com/adms/test/TestApp.java
+++ src/test/java/com/adms/test/TestApp.java
@@ -1,5 +1,10 @@
 package com.adms.test;
 
+import java.util.Arrays;
+import java.util.Calendar;
+
+import com.adms.utils.DateUtil;
+
 
 
 public class TestApp {
@@ -8,6 +13,17 @@ public class TestApp {
 
 		try {
 			System.out.println("start");
+			
+			
+			
+			Calendar cal = Calendar.getInstance();
+			System.out.println(DateUtil.convDateToString("MMM_yyyyMM", cal.getTime()));
+			
+			String[] plan1s = new String[]{"PLAN 1", "600000"};
+			
+			String planVal = "40000-Plan 1";
+			
+			System.out.println(Arrays.binarySearch(plan1s, planVal.toUpperCase()));
 			
 			
 			
